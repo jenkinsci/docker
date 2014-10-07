07c307dac54008a174d45564caf9f2a9f82a6156
Nicolas De Loof <nicolas.deloof@gmail.com>
2014-10-07 14:02:42 +0200

    Conform to [dockerfile best practices](https://docs.docker.com/articles/dockerfile_best-practices/#entrypoint) to let user run alternate command but still detect user passing jenkins launcher custom arguments
    default usage
    
    	➜ docker run --rm -it jenkins 
    	Running from: /usr/share/jenkins/jenkins.war
    	webroot: EnvVars.masterEnvVars.get("JENKINS_HOME")
    	Oct 07, 2014 12:06:48 PM winstone.Logger logInternal
    	INFO: Beginning extraction from war file
        ...
    
    
    detect jenkins launcher arguments
    
    	➜ docker run --rm -it jenkins --version
    	Running from: /usr/share/jenkins/jenkins.war
    	webroot: EnvVars.masterEnvVars.get("JENKINS_HOME")
    	1.565.3
    
    run alternate command
    
        ➜ docker run --rm -it jenkins bash
    	jenkins@24c6215c8492:/$

--- jenkins.sh
+++ jenkins.sh
@@ -1,4 +1,10 @@
 #! /bin/bash
 
-exec java $JAVA_OPTS -jar /usr/share/jenkins/jenkins.war $JENKINS_OPTS --prefix=$JENKINS_PREFIX $@
+# if `docker run` first argument start with `--` the user is passing jenkins launcher arguments
+if [[ $# -lt 1 ]] || [[ "$1" == "--"* ]]; then
+   exec java $JAVA_OPTS -jar /usr/share/jenkins/jenkins.war $JENKINS_OPTS --prefix=$JENKINS_PREFIX $@
+fi
+
+# As argument is not jenkins, assume user want to run his own process, for sample a `bash` shell to explore this image
+exec $@
 
