40c3e3f46939b9f9dcf8d46e62fa7daa80485588
Nicolas De Loof <nicolas.deloof@gmail.com>
2014-12-02 17:19:06 +0100

    fix distinct download URL for weekly vs stable

--- update.sh
+++ update.sh
@@ -11,6 +11,11 @@ for version in "${versions[@]}"; do
 	rm -rf "$version"/*
 	cp Dockerfile.template jenkins.sh plugins.sh init.groovy "$version/"
 	mv "$version/Dockerfile.template" "$version/Dockerfile"
-	sed -i "" 's/%%JENKINS_VERSION%%/'$version'/g; s/%%JENKINS_VERSION%%/'$version'/g' "$version/Dockerfile"
+    case $version in
+    	1\.*\.*) download="http:\/\/mirrors.jenkins-ci.org\/war-stable\/$version\/jenkins.war" ;;
+    	*) download="http:\/\/mirrors.jenkins-ci.org\/war\/$version\/jenkins.war" ;;
+	esac
+
+	sed -i "" 's/%%JENKINS_VERSION%%/'$version'/g; s/%%DOWNLOAD_URL%%/'$download'/g' "$version/Dockerfile"
 done
 
