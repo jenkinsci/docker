a679ceeb037a74b2c53e739b2e3398cc2eec7744
Nicolas De Loof <nicolas.deloof@gmail.com>
2014-12-03 09:02:33 +0100

    Revert "add support for Jenkins Enterprise by cloudbees"
    This reverts commit 91a58f1b7341ab8a253de7f4dec752d9daebd27b.

--- update.sh
+++ update.sh
@@ -12,7 +12,6 @@ for version in "${versions[@]}"; do
 	cp Dockerfile.template jenkins.sh plugins.sh init.groovy "$version/"
 	mv "$version/Dockerfile.template" "$version/Dockerfile"
     case $version in
-		1\.*\.*\.*) download="http:\/\/nectar-downloads.cloudbees.com\/jenkins-enterprise\/${version::5}\/war\/$version\/jenkins.war" ;;
     	1\.*\.*) download="http:\/\/mirrors.jenkins-ci.org\/war-stable\/$version\/jenkins.war" ;;
     	*) download="http:\/\/mirrors.jenkins-ci.org\/war\/$version\/jenkins.war" ;;
 	esac
