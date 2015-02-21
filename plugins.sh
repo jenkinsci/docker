dddb965c1577ebe9862fb5010f0ad75f1ea883fb
Manuel Prinz <manuel@pinguinkiste.de>
2015-02-21 13:03:29 +0100

    Ignore comments and empty lines in plugins file

--- plugins.sh
+++ plugins.sh
@@ -13,5 +13,7 @@ mkdir -p $REF
 
 while read spec; do
     plugin=(${spec//:/ }); 
+    [[ ${plugin[0]} =~ ^# ]] && continue
+    [[ ${plugin[0]} =~ ^\s*$ ]] && continue
     curl -L ${JENKINS_UC}/download/plugins/${plugin[0]}/${plugin[1]}/${plugin[0]}.hpi -o $REF/${plugin[0]}.hpi;
 done  < $1
