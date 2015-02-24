0a1cf2354895366d34a7aa3c14781bdc37973669
hayderimran7 <hayderimran7@gmail.com>
2015-02-23 23:16:27 -0800

    add ability to add comments to plugins.txt file

--- plugins.sh
+++ plugins.sh
@@ -12,6 +12,8 @@ REF=/usr/share/jenkins/ref/plugins
 mkdir -p $REF
 
 while read spec; do
+# Skip the comments starting with #
+    [[ ${spec:0:1} == '#' ]] && continue
     plugin=(${spec//:/ }); 
     curl -L ${JENKINS_UC}/download/plugins/${plugin[0]}/${plugin[1]}/${plugin[0]}.hpi -o $REF/${plugin[0]}.hpi;
 done  < $1
