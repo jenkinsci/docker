342ee2a4b4af8ca5fc0a205cebef6464d5a28394
Nicolas De Loof <nicolas.deloof@gmail.com>
2015-03-18 08:02:43 +0100

    fix plugin.sh script

--- 1.584/plugins.sh
+++ 1.584/plugins.sh
@@ -5,7 +5,7 @@
 # 
 # FROM jenkins
 # COPY plugins.txt /plugins.txt
-# RUN /usr/share/jenkins/plugins.sh /plugins.txt
+# RUN /usr/local/bin/plugins.sh /plugins.txt
 # 
 
 REF=/usr/share/jenkins/ref/plugins
@@ -13,5 +13,7 @@ mkdir -p $REF
 
 while read spec; do
     plugin=(${spec//:/ }); 
+    [[ ${plugin[0]} =~ ^# ]] && continue
+    [[ ${plugin[0]} =~ ^\s*$ ]] && continue
     curl -L ${JENKINS_UC}/download/plugins/${plugin[0]}/${plugin[1]}/${plugin[0]}.hpi -o $REF/${plugin[0]}.hpi;
 done  < $1
