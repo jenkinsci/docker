1bdf7cabcc49663dd174c470c2f2c08b4c3dcccb
Nicolas De Loof <nicolas.deloof@gmail.com>
2015-03-18 08:03:41 +0100

    move to a simpler tag model as discussed on https://github.com/docker-library/official-images/pull/544

--- 1.577/init.groovy
+++
@@ -1,9 +0,0 @@
-import hudson.model.*;
-import jenkins.model.*;
-
-
-Thread.start {
-      sleep 10000
-      println "--> setting agent port for jnlp"
-      Jenkins.instance.setSlaveAgentPort(50000)
-}
