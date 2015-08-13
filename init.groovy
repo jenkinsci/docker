795a8f94ab7827199605d3ed8c9d57a2f4584776
Marcel Huber <marcelhuberfoo@gmail.com>
2015-08-13 15:55:39 +0200

    Corrected access to env and added type conversion

--- init.groovy
+++ init.groovy
@@ -1,10 +1,10 @@
 import hudson.model.*;
 import jenkins.model.*;
 
-
 Thread.start {
       sleep 10000
+      def env = System.getenv()
       println "--> setting agent port for jnlp"
-      Jenkins.instance.setSlaveAgentPort(env['JENKINS_SLAVE_AGENT_PORT'])
+      Jenkins.instance.setSlaveAgentPort(env['JENKINS_SLAVE_AGENT_PORT'].toInteger())
       println "--> setting agent port for jnlp... done"
 }
