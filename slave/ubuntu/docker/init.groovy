6ffda9daddb2af5bf5deb6a886ff6e40ad65b450
Victor Vitayaudom <victorv@groupon.com>
2015-08-24 11:51:42 -0500

    run jenkins daemon with jnlp /computer/slave1 that has docker and docker-compose installed via ``$ docker-compose -f slave/ubuntu/docker/docker-compose.yml up -d``

---
+++ slave/ubuntu/docker/init.groovy
@@ -0,0 +1,42 @@
+import hudson.model.*;
+import hudson.slaves.*;
+import jenkins.model.*;
+
+Thread.start {
+      sleep 10000
+      def env = System.getenv()
+      def instance = Jenkins.getInstance()
+
+      // avoid overloading jenkins master
+      println "--> setting jenkins master to 0 executors"
+      instance.setNumExecutors(0)
+      println "--> setting jenkins master to 0 executors... done"
+
+      // modify below to your needs
+      println "--> adding JNLP slave(s)"
+      List<String> clients = new ArrayList<String>()
+      clients.add("slave1")
+      // clients.add("...")
+
+      // http://javadoc.jenkins-ci.org/hudson/slaves/DumbSlave.html
+      for (String client : clients) {
+        println "--> creating 2 executor stub for /computer/" + client + " available via /label/docker based on java:8u45-jdk with docker and docker-compose"
+        instance.addNode(
+	  new DumbSlave(
+		client,
+                "JNLP slave with docker and docker-compose",
+                "/root",
+                "5",
+                Node.Mode.NORMAL,
+                "docker",
+                new JNLPLauncher(),
+                new RetentionStrategy.Always(),
+                new LinkedList()
+          )
+        )
+      }
+      println "--> adding JNLP slave(s)... done"
+
+      instance.save()
+      println "--> saving " + env['JENKINS_HOME'] + "/config.xml... done"
+}
