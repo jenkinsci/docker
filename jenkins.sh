db6eb677da5141c17080c5bd1fe6204e1e0637f9
Sergey Chipiga <chipiga86@yandex.ru>
2016-02-02 00:07:21 +0300

    use /etc/init.d/jenkins variant to provide multiple java options with quotes

--- jenkins.sh
+++ jenkins.sh
@@ -29,16 +29,7 @@ find /usr/share/jenkins/ref/ -type f -exec bash -c "copy_reference_file '{}'" \;
 
 # if `docker run` first argument start with `--` the user is passing jenkins launcher arguments
 if [[ $# -lt 1 ]] || [[ "$1" == "--"* ]]; then
-  CMD=(java)
-  if [ ! -z "$JAVA_OPTS" ]; then
-    CMD+=("$JAVA_OPTS")
-  fi
-  CMD+=(-jar)
-  CMD+=(/usr/share/jenkins/jenkins.war)
-  if [ ! -z "$JENKINS_OPTS" ]; then
-    CMD+=("$JENKINS_OPTS")
-  fi
-  exec "${CMD[@]}" "$@"
+  exec /bin/bash -c "java $JAVA_OPTS -jar /usr/share/jenkins/jenkins.war $JENKINS_OPTS $@"
 fi
 
 # As argument is not jenkins, assume user want to run his own process, for sample a `bash` shell to explore this image
