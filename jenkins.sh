e7d56fa71d44ace1f1d8258ef216bba64f266cca
Carlos Sanchez <carlos@apache.org>
2016-02-29 13:55:33 +0100

    Fix Issue #213 Get JAVA_OPTS, JENKIS_OPTS and arguments working
    With the right quoting

--- jenkins.sh
+++ jenkins.sh
@@ -29,7 +29,7 @@ find /usr/share/jenkins/ref/ -type f -exec bash -c "copy_reference_file '{}'" \;
 
 # if `docker run` first argument start with `--` the user is passing jenkins launcher arguments
 if [[ $# -lt 1 ]] || [[ "$1" == "--"* ]]; then
-  exec java $JAVA_OPTS -jar /usr/share/jenkins/jenkins.war $JENKINS_OPTS "$@"
+  eval "exec java $JAVA_OPTS -jar /usr/share/jenkins/jenkins.war $JENKINS_OPTS \"\$@\""
 fi
 
 # As argument is not jenkins, assume user want to run his own process, for sample a `bash` shell to explore this image
