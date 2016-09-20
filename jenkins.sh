ffbd787bd6373d03418c0a09471cebc55b5d5301
Charles Duffy <chaduffy@cisco.com>
2016-09-20 11:51:35 -0500

    Fix shellcheck warning
    Quoting arguments as in `: "${varname:=default}"` increases usefulness of `set -x` logs.

--- jenkins.sh
+++ jenkins.sh
@@ -1,7 +1,7 @@
 #! /bin/bash -e
 
-: ${JENKINS_HOME:="/var/jenkins_home"}
-touch "${COPY_REFERENCE_FILE_LOG}" || (echo "Can not write to ${COPY_REFERENCE_FILE_LOG}. Wrong volume permissions?" && exit 1)
+: "${JENKINS_HOME:="/var/jenkins_home"}"
+touch "${COPY_REFERENCE_FILE_LOG}" || { echo "Can not write to ${COPY_REFERENCE_FILE_LOG}. Wrong volume permissions?"; exit 1; }
 echo "--- Copying files at $(date)" >> "$COPY_REFERENCE_FILE_LOG"
 find /usr/share/jenkins/ref/ -type f -exec bash -c '. /usr/local/bin/jenkins-support; for arg; do copy_reference_file "$arg"; done' _ {} +
 
