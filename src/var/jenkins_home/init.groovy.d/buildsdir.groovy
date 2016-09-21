e656afc8b55473461f202359f11c894999ccf013
Flavio Aiello <flavio.aiello@swisscom.com>
2016-09-21 08:48:39 +0200

    fixed buildsdir script

--- src/var/jenkins_home/init.groovy.d/buildsdir.groovy
+++ src/var/jenkins_home/init.groovy.d/buildsdir.groovy
@@ -4,7 +4,7 @@ import jenkins.model.*;
 Thread.start {
       sleep 10000
       println "--> setting builds root directory"
-      string buildsDir = System.getenv('JENKINS_BUILDSDIR') ? System.getenv('JENKINS_BUILDSDIR').toString() : "${JENKINS_HOME}/builds/${ITEM_FULL_NAME}"
+      def buildsDir = System.getenv('JENKINS_BUILDSDIR') ? System.getenv('JENKINS_BUILDSDIR') : "${JENKINS_HOME}/builds/${ITEM_FULL_NAME}"
       Jenkins.instance.setRawBuildsDir(buildsDir)
       println "--> setting builds root directory... done"
 }
