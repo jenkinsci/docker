837f9a69ba9b2b607fd427b6ad3eea3344a8fe9a
Charles Duffy <chaduffy@cisco.com>
2016-09-27 16:14:25 -0500

    Fix regression from bbbb76d4
    Code tested against BSD xargs, fails with GNU implementation.

--- jenkins.sh
+++ jenkins.sh
@@ -12,12 +12,12 @@ if [[ $# -lt 1 ]] || [[ "$1" == "--"* ]]; then
   java_opts_array=()
   while IFS= read -r -d '' item; do
     java_opts_array+=( "$item" )
-  done < <(xargs printf '%s\0' <<<"$JAVA_OPTS")
+  done < <([[ $JAVA_OPTS ]] && xargs printf '%s\0' <<<"$JAVA_OPTS")
 
   jenkins_opts_array=( )
   while IFS= read -r -d '' item; do
     jenkins_opts_array+=( "$item" )
-  done < <(xargs printf '%s\0' <<<"$JENKINS_OPTS")
+  done < <([[ $JENKINS_OPTS ]] && xargs printf '%s\0' <<<"$JENKINS_OPTS")
 
   exec java "${java_opts_array[@]}" -jar /usr/share/jenkins/jenkins.war "${jenkins_opts_array[@]}" "$@"
 fi
