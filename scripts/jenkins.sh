880c0b58731c5f208c7cbef5c27393276e30f96e
Hosh Sadiq <superaktieboy@gmail.com>
2017-03-22 23:28:48 +0000

    Allow for java to calculate correct limits

--- scripts/jenkins.sh
+++ scripts/jenkins.sh
@@ -5,6 +5,9 @@ touch "${COPY_REFERENCE_FILE_LOG}" || { echo "Can not write to ${COPY_REFERENCE_
 echo "--- Copying files at $(date)" >> "$COPY_REFERENCE_FILE_LOG"
 find /usr/share/jenkins/ref/ -type f -exec bash -c '. /usr/local/bin/jenkins-support; for arg; do copy_reference_file "$arg"; done' _ {} +
 
+source "/usr/local/bin/container-limits"
+JAVA_OPTS="$JAVA_OPTS $(/usr/local/bin/java-default-options)"
+
 # if `docker run` first argument start with `--` the user is passing jenkins launcher arguments
 if [[ $# -lt 1 ]] || [[ "$1" == "--"* ]]; then
 
