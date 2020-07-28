de2539fe5dfd31f4cde4ceca44fedd562ad51160
Tim Jacomb <timjacomb1+github@gmail.com>
2020-07-28 16:05:52 +0100

    Shellcheck fix

--- jenkins-plugin-cli.sh
+++ jenkins-plugin-cli.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-JENKINS_UC=${JENKINS_UC_OVERRIDE:-https://updates.jenkins.io/update-center.actual.json}
-JENKINS_UC_EXPERIMENTAL=${JENKINS_UC_EXPERIMENTAL_OVERRIDE:-https://updates.jenkins.io/experimental/update-center.actual.json}
+export JENKINS_UC=${JENKINS_UC_OVERRIDE:-https://updates.jenkins.io/update-center.actual.json}
+export JENKINS_UC_EXPERIMENTAL=${JENKINS_UC_EXPERIMENTAL_OVERRIDE:-https://updates.jenkins.io/experimental/update-center.actual.json}
 
 java -jar /usr/lib/jenkins-plugin-manager.jar "$@"
