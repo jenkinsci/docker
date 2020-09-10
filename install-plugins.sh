d6ecdcfebfcf13a639257001688dcff8a77d9e58
Alex Earl <slide.o.mix@gmail.com>
2020-09-10 05:10:02 -0700

    Fix typo

--- install-plugins.sh
+++ install-plugins.sh
@@ -20,7 +20,7 @@
 
 set -o pipefail
 
-echo "WARN: install-plugins.sh is deprecated, please switch to jenkins-plugins-cli.sh"
+echo "WARN: install-plugins.sh is deprecated, please switch to jenkins-plugin-cli.sh"
 
 JENKINS_WAR=${JENKINS_WAR:-/usr/share/jenkins/jenkins.war}
 
