0e8271bf693bddcaa76cfdedb8ef5d8ae940b859
Philippe scorsolini <pscorso93@gmail.com>
2020-10-22 12:45:14 +0200

    ignore SC2086 as in previous usage

--- install-plugins.sh
+++ install-plugins.sh
@@ -246,6 +246,7 @@ main() {
 
     # Get the update center URL based on the jenkins version
     jenkinsVersion="$(jenkinsMajorMinorVersion)"
+    # shellcheck disable=SC2086
     jenkinsUcJson=$(curl ${CURL_OPTIONS:--sSfL} -o /dev/null -w "%{url_effective}" "${JENKINS_UC}/update-center.json?version=${jenkinsVersion}")
     if [ -n "${jenkinsUcJson}" ]; then
         JENKINS_UC_LATEST=${jenkinsUcJson//update-center.json/}
