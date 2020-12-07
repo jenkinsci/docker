743110385469e574457512dade672e2df8f3ea11
Gareth Evans <gareth@bryncynfelin.co.uk>
2020-12-07 14:26:43 +0000

    chore: invert check to favour 'docker login' if DOCKER_CONFIG is set

--- .ci/common-functions.sh
+++ .ci/common-functions.sh
@@ -1,12 +1,12 @@
 docker-login() {
-    if [ -n "$DOCKERHUB_USERNAME" ] && [ -n "$DOCKERHUB_PASSWORD"]; then
-      # Using username and password variables
-      echo "Using username/password"
-      docker login --username ${DOCKERHUB_USERNAME} --password ${DOCKERHUB_PASSWORD}
-    else
+    if [ -n "$DOCKER_CONFIG" ]; then
       # Making use of the credentials stored in `config.json`
       echo "Using config.json"
       docker login
+    else
+      # Using username and password variables
+      echo "Using username/password"
+      docker login --username ${DOCKERHUB_USERNAME} --password ${DOCKERHUB_PASSWORD}
     fi
     echo "Docker logged in successfully"
 }
