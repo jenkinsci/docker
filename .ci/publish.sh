e2dff829de6aa7dc84736b87f6921ad5dce3bb1f
Mark Waite <mark.earl.waite@gmail.com>
2021-07-28 10:11:36 -0600

    Force 2.289.3 to be published (#1164)
    Some variants have already published but not all
    
    This change will be reverted once 2.289.3 images are published

--- .ci/publish.sh
+++ .ci/publish.sh
@@ -127,7 +127,8 @@ latest_weekly_version=$(echo "${versions}" | tail -n 1)
 latest_lts_version=$(echo "${versions}" | grep -E '[0-9]\.[0-9]+\.[0-9]' | tail -n 1 || echo "No LTS versions")
 
 for version in $versions; do
-    if is-published "$version$variant"; then
+    # Temporary hack to publish 2.289.3 even though some variants are already published
+    if is-published "$version$variant" && [ "$version" != "2.289.3" ] ; then
         echo "Tag is already published: $version$variant"
     else
         echo "$version$variant not published yet"
