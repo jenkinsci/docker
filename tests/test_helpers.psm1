87b67c6ce63a9692f6eb6fe74d45eb84699d693f
Axel Müller <axel.mueller@avanux.de>
2023-01-23 17:25:30 +0100

    run test only on Linux

--- tests/test_helpers.psm1
+++ tests/test_helpers.psm1
@@ -22,10 +22,6 @@ if(-Not (Test-CommandExists docker)) {
     Write-Error "docker is not available"
 }
 
-if(-Not (Test-CommandExists ps)) {
-    Write-Error "ps is not available"
-}
-
 function Retry-Command {
     [CmdletBinding()]
     param (
