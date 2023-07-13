0066bbb8b926122cf901a860d2f9c0f932f567e5
Anoint <72187543+whoami-anoint@users.noreply.github.com>
2023-07-13 15:56:13 +0530

    Update jenkins-plugin-cli.sh
        Replaced the usage of xargs with a direct array assignment to avoid potential vulnerabilities.
        Used the read command to read the JAVA_OPTS environment variable into an array.
        Ensured proper handling of spaces and special characters within the JAVA_OPTS string.
        Passed the java_opts_array array as arguments to the java command.
        Retained the -jar option and the path to the jenkins-plugin-manager.jar file.
        Preserved the ability to pass additional command-line arguments using the $@ variable.
        Overall, the updated code is more effective and reliable in handling the Java options and executing the Java program.

--- jenkins-plugin-cli.sh
+++ jenkins-plugin-cli.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-# read JAVA_OPTS into array to avoid need for eval (and associated vulnerabilities)
-java_opts_array=()
-while IFS= read -r -d '' item; do
-	java_opts_array+=( "$item" )
-done < <([[ $JAVA_OPTS ]] && xargs printf '%s\0' <<<"$JAVA_OPTS")
+# Read JAVA_OPTS into array to avoid the need for eval and handle spaces correctly
+read -r -a java_opts_array <<<"$JAVA_OPTS"
 
+# Execute the Java program
 exec java "${java_opts_array[@]}" -jar /opt/jenkins-plugin-manager.jar "$@"
+
