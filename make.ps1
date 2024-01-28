1c0d1f84a4a32910d8d421de4baa0558ea4ba1a2
Mark Waite <mark.earl.waite@gmail.com>
2024-01-28 15:02:54 -0700

    Use Jenkins 2.442 as default version (#1821)
    Builds generally specify the Jenkins version with an argument, but it
    seems reasonable to prefer a default version that does not have known
    security vulnerabilities.

--- make.ps1
+++ make.ps1
@@ -2,7 +2,7 @@
 Param(
     [Parameter(Position=1)]
     [String] $Target = 'build',
-    [String] $JenkinsVersion = '2.431',
+    [String] $JenkinsVersion = '2.442',
     [switch] $DryRun = $false
 )
 
