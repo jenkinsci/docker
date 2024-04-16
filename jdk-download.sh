f45ce7cf925b88addbac877ea6304157ebea321d
gounthar <gounthar@gmail.com>
2024-04-16 14:04:44 +0200

    fix(docker): Hadolint shellcheck.

--- jdk-download.sh
+++ jdk-download.sh
@@ -8,5 +8,5 @@ DASHED_JAVA_VERSION=${JAVA_VERSION//./-} \
   && CONVERTED_ARCH=$(arch | sed -e 's/x86_64/x64/' -e 's/armv7l/arm/') \
   && wget --quiet https://github.com/adoptium/temurin"${JAVA_MAJOR_VERSION}"-binaries/releases/download/jdk-"${ENCODED_ARCHIVE_DIRECTORY}"/OpenJDK"${JAVA_MAJOR_VERSION}"U-jdk_"${CONVERTED_ARCH}"_linux_hotspot_"${JAVA_VERSION}".tar.gz -O /tmp/jdk.tar.gz \
   && tar -xzf /tmp/jdk.tar.gz -C /opt/ \
-  && mv /opt/jdk-"${ARCHIVE_DIRECTORY}" /opt/jdk-${JAVA_VERSION} \
+  && mv /opt/jdk-"${ARCHIVE_DIRECTORY}" /opt/jdk-"${JAVA_VERSION}" \
   && rm -f /tmp/jdk.tar.gz
