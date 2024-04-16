464d694d90c61085f3eb9093b12f3b9084a8038e
gounthar <gounthar@gmail.com>
2024-04-16 14:03:14 +0200

    fix(docker): Hadolint shellcheck.

--- jdk-download-for-alpine.sh
+++ jdk-download-for-alpine.sh
@@ -1,3 +1,4 @@
+# shellcheck shell=dash
 #!/usr/bin/env ash
 JAVA_MAJOR_VERSION=$(echo "$JAVA_VERSION" | cut -d'+' -f1 | cut -d'.' -f1) \
   && ARCHIVE_DIRECTORY=$(echo "$JAVA_VERSION" | sed -e 's/_/+/') \
@@ -5,5 +6,5 @@ JAVA_MAJOR_VERSION=$(echo "$JAVA_VERSION" | cut -d'+' -f1 | cut -d'.' -f1) \
   && CONVERTED_ARCH=$(arch | sed -e 's/x86_64/x64/' -e 's/armv7l/arm/') \
   && wget --quiet https://github.com/adoptium/temurin"${JAVA_MAJOR_VERSION}"-binaries/releases/download/jdk-"${ENCODED_ARCHIVE_DIRECTORY}"/OpenJDK"${JAVA_MAJOR_VERSION}"U-jdk_"${CONVERTED_ARCH}"_alpine-linux_hotspot_"${JAVA_VERSION}".tar.gz -O /tmp/jdk.tar.gz \
   && tar -xzf /tmp/jdk.tar.gz -C /opt/ \
-  && mv /opt/jdk-"${ARCHIVE_DIRECTORY}" /opt/jdk-${JAVA_VERSION} \
+  && mv /opt/jdk-"${ARCHIVE_DIRECTORY}" /opt/jdk-"${JAVA_VERSION}" \
   && rm -f /tmp/jdk.tar.gz
