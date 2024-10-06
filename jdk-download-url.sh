9406a47b0203e8efff3f8360d4bf50faba9864f4
Mark Waite <mark.earl.waite@gmail.com>
2024-10-06 06:43:12 -0600

    Include URL in download failure error message
    Much easier to diagnose failures if the requesting URL is included in
    the message.  Since the failure will end that portion of the build job,
    the extra verbosity is not additionally harmful.

--- jdk-download-url.sh
+++ jdk-download-url.sh
@@ -74,7 +74,7 @@ for ARCH in $ARCHS; do
     URL="https://api.adoptium.net/v3/binary/version/jdk-${ENCODED_ARCHIVE_DIRECTORY}/${OS_TYPE}/${ARCH}/jdk/hotspot/normal/eclipse?project=jdk"
 
     if ! RESPONSE=$(curl -fsI "$URL"); then
-        echo "Error: Failed to fetch the URL for architecture ${ARCH}. Exiting with status 1." >&2
+        echo "Error: Failed to fetch the URL for architecture ${ARCH} from ${URL}. Exiting with status 1." >&2
         echo "Response: $RESPONSE" >&2
         exit 1
     fi
@@ -84,7 +84,7 @@ for ARCH in $ARCHS; do
 
     # If no redirect URL was found, exit the script with an error message
     if [ -z "$REDIRECTED_URL" ]; then
-        echo "Error: No redirect URL found for architecture ${ARCH}. Exiting with status 1." >&2
+        echo "Error: No redirect URL found for architecture ${ARCH} from ${URL}. Exiting with status 1." >&2
         echo "Response: $RESPONSE" >&2
         exit 1
     fi
