4f899640a0b648fc3017341ca07cfe78601450cb
Hervé Le Meur <lemeurherve.jenkins@gmail.com>
2025-10-27 20:54:17 +0100

    fix Compare-VersionLessThan

--- jenkins-support.psm1
+++ jenkins-support.psm1
@@ -17,11 +17,18 @@ function Compare-VersionLessThan([string] $version1 = '', [string] $version2 = '
     if ($version1Parts[0] -lt $version2parts[0]) {
         return $true
     }
+    if ($version1Parts[0] -gt $version2parts[0]) {
+        return $false
+    }
 
     $maxLength = [Math]::Max($version1Parts.Length, $version2parts.Length)
-    for ($i = 0; $i -lt $maxLength; $i++) {
-        $part1 = if ($i -lt $version1Parts.Length) { $version1Parts[$i] } else { '0' }
-        $part2 = if ($i -lt $version2Parts.Length) { $version2Parts[$i] } else { '0' }
+    for ($i = 1; $i -lt $maxLength; $i++) {
+        $version1part = if ($i -lt $version1Parts.Length) { $version1Parts[$i] } else { '0' }
+        $version2part = if ($i -lt $version2Parts.Length) { $version2Parts[$i] } else { '0' }
+
+        if ($version1part -eq $version2part) {
+            continue
+        }
 
         # Security fix backport special case
         # Ex: 3894.vd0f0248b_a_fc4 < 3894.3896.vca_2c931e7935
@@ -29,14 +36,12 @@ function Compare-VersionLessThan([string] $version1 = '', [string] $version2 = '
         # -> security fix backport adds the backport source first part as second part
         # https://github.com/jenkinsci/workflow-cps-plugin/releases/tag/3894.vd0f0248b_a_fc4
         # https://github.com/jenkinsci/workflow-cps-plugin/releases/tag/3894.3896.vca_2c931e7935
-        if ($part1 -eq $part2) {
-            # If only the second part of $1 starts with a "v", then $1 is older
-            if ($part1.Substring(0,1) -eq 'v' -and $part2.Substring(0,1) -ne 'v') {
-                return $true
-            }
+        # If only the second part of $1 starts with a "v", then $1 is older
+        if ($version1part.Substring(0,1) -eq 'v' -and $version2part.Substring(0,1) -ne 'v') {
+            return $true
         }
 
-        if ($part1 -lt $part2) {
+        if ($version1part -lt $version2part) {
             return $true
         } else {
             return $false
