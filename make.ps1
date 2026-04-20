81c790bc7404a46718f98a85cbd1f429457f4642
Hervé Le Meur <91831478+lemeurherve@users.noreply.github.com>
2026-04-20 19:42:09 +0200

    Revert "hotfix: pass `-SkipPublisherCheck` parameter to Pester module install…"
    This reverts commit 4e3b9598750bc63655d07a0f611f6d361af03366.

--- make.ps1
+++ make.ps1
@@ -259,8 +259,7 @@ if ($target -eq 'test') {
         $mod = Get-InstalledModule -Name Pester -MinimumVersion 5.3.0 -MaximumVersion 5.3.3 -ErrorAction SilentlyContinue
         if ($null -eq $mod) {
             Write-Host '= TEST: Pester 5.3.x not found: installing...'
-            # TODO: remove -SkipPublisherCheck when Pester 3.4.0 isn't present by default in Windows agents images anymore
-            Install-Module -Force -Name Pester -MaximumVersion 5.3.3 -Scope CurrentUser -SkipPublisherCheck
+            Install-Module -Force -Name Pester -MaximumVersion 5.3.3 -Scope CurrentUser
         }
 
         Import-Module Pester
